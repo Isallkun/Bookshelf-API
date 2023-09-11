bf628f2be6c4fa021cc0732da4e8c5254262bd58
santriinsyaf <aswadalansori@gmail.com>
2023-09-12 06:21:50 +0700

    fixed an error when running eslint

--- src/server.js
+++ src/server.js
@@ -1,21 +1,26 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
-    host: "localhost",
+    host: 'localhost',
     routes: {
       cors: {
-        origin: ["*"],
+        origin: ['*'],
       },
     },
   });
 
   server.route(routes);
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  try {
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+  } catch (error) {
+    console.error('Terjadi kesalahan saat memulai server:', error);
+    process.exit(1);
+  }
 };
 
 init();
